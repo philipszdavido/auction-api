1ad20ffe9d524006788153d04e5bdc3e9a07267f
philipszdavido <kurtwanger40@gmail.com>
2024-03-14 14:24:54 +0100

    Refactored auth endpoints

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import { User, create, find } from "./mock/user.db";
 import { comparePassword, hashPassword, signToken } from "./utils/auth";
+import authRoutes from "./routes/authRoutes";
 
 dotenv.config();
 
@@ -10,55 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post("/register", async (req, res) => {
-  const { username, password } = req.body;
-
-  if (find(username)) {
-    return res.status(201).json({ message: "Invalid username" });
-  }
-
-  try {
-    const hashedPassword = await hashPassword(password);
-
-    create(username, hashedPassword);
-
-    const token = signToken({ username, password });
-
-    res.status(200).json({ token });
-  } catch (error) {
-    console.error(error);
-    res.status(201).json({ message: "Invalid username or password" });
-  }
-});
-
-app.post("/login", async (req: Request, res: Response) => {
-  const { username, password } = req.body;
-
-  try {
-    const user = find(username) as User;
-
-    if (!user) {
-      return res.status(201).json({ message: "Invalid username" });
-    }
-
-    const passwordMatch = await comparePassword(password, user.hashPassword);
-
-    if (!passwordMatch) {
-      return res.status(201).json({ message: "Wrong password" });
-    }
-
-    const token = signToken({ username, password });
-
-    return res.json({ token });
-  } catch (error) {
-    console.error(error);
-    return res.status(201).json({ message: "Login failed" });
-  }
-});
-
-app.post("/logout", (req: Request, res: Response) => {
-  res.json({ message: "Logout success" });
-});
+app.use("/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
