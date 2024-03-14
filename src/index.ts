6e11f557095c6bf500b26a6a32f1bcb91c04a664
philipszdavido <kurtwanger40@gmail.com>
2024-03-14 14:14:40 +0100

    Migrated auth methods

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,24 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import bcrypt from "bcrypt";
-import { create, find } from "./mock/user.db";
-import { signToken } from "./utils/auth";
+import { User, create, find } from "./mock/user.db";
+import { comparePassword, hashPassword, signToken } from "./utils/auth";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(
-  express.json({
-    type: ["application/json", "text/plain"],
-  })
-);
+app.use(express.json());
 
 app.post("/register", async (req, res) => {
-  console.log(req.body);
-
   const { username, password } = req.body;
 
+  if (find(username)) {
+    return res.status(201).json({ message: "Invalid username" });
+  }
+
   try {
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await hashPassword(password);
 
     create(username, hashedPassword);
 
@@ -29,29 +26,37 @@ app.post("/register", async (req, res) => {
 
     res.status(200).json({ token });
   } catch (error) {
+    console.error(error);
     res.status(201).json({ message: "Invalid username or password" });
   }
 });
 
-app.post("/login", (req: Request, res: Response) => {
+app.post("/login", async (req: Request, res: Response) => {
   const { username, password } = req.body;
 
   try {
-    const user = find(username);
+    const user = find(username) as User;
 
-    if (user && user.hashPassword === password) {
-      const token = signToken({ username, password });
-
-      return res.json({ token });
+    if (!user) {
+      return res.status(201).json({ message: "Invalid username" });
     }
 
-    return res.status(200).json({ message: "Invalid username or password" });
+    const passwordMatch = await comparePassword(password, user.hashPassword);
+
+    if (!passwordMatch) {
+      return res.status(201).json({ message: "Wrong password" });
+    }
+
+    const token = signToken({ username, password });
+
+    return res.json({ token });
   } catch (error) {
-    return res.status(201).json({ message: "Invalid username or password" });
+    console.error(error);
+    return res.status(201).json({ message: "Login failed" });
   }
 });
 
-app.post("/logout", (req, res) => {
+app.post("/logout", (req: Request, res: Response) => {
   res.json({ message: "Logout success" });
 });
 
