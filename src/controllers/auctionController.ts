15ce0a7d5d7bac7419ffcfcb79ac5d9e6c75f1dc
philipszdavido <kurtwanger40@gmail.com>
2024-03-18 16:42:25 +0100

    Added controller logic for the statistics route

--- src/controllers/auctionController.ts
+++ src/controllers/auctionController.ts
@@ -12,6 +12,16 @@ export const submitBid = (req: Request, res: Response) => {
   res.json({ message: "Submit bid" });
 };
 
-export const statistics = (req: Request, res: Response) => {
-  res.json({ message: "Statistics" });
+export const statistics = async (req: Request, res: Response) => {
+  try {
+    const stats = await statisticsFn();
+
+    res.status(200).json({
+      message: "Statistics",
+      data: convertBigIntToString(stats),
+    });
+  } catch (error) {
+    console.error("Error getting statistics:", error);
+    res.status(500).json({ message: "Error getting statistics", error });
+  }
 };
