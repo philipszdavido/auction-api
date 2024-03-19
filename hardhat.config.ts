f290dce2711fe3744ba4d35378493517dd5b6209
philipszdavido <kurtwanger40@gmail.com>
2024-03-19 09:42:08 +0100

    Set up networks on hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,6 +8,17 @@ const { ETHEREUM_NETWORK, SIGNER_PRIVATE_KEY } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  defaultNetwork: "localhost",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      accounts: [String(SIGNER_PRIVATE_KEY)],
+    },
+    alchemy: {
+      url: String(ETHEREUM_NETWORK),
+      accounts: [String(SIGNER_PRIVATE_KEY)],
+    },
+  },
 };
 
 export default config;
