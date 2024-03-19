86c620a89682f8ea1fe4b8cf67f8125832cb1a8c
philipszdavido <kurtwanger40@gmail.com>
2024-03-19 09:42:54 +0100

    Added deploy script for hardhat

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,52 +1,30 @@
-import { Web3 } from "web3";
-import artifacts from "../artifacts/contracts/SimpleAuction.sol/SimpleAuction.json";
+import { ethers } from "hardhat";
 import dotenv from "dotenv";
-import { getNetworkUrl } from "../src/auctionContract/contract";
 
-const deploy = async () => {
+async function main() {
   dotenv.config();
 
-  console.log("Deploying contract to Infura");
+  const { BENEFICIARY_ADDRESS, AUCTION_END_TIME } = process.env;
 
-  const { abi, bytecode } = artifacts;
+  const beneficiaryAddress = String(BENEFICIARY_ADDRESS);
+  const auctionEndTime = Number(AUCTION_END_TIME);
 
-  const network = process.env.ETHEREUM_NETWORK;
+  const SimpleAuction = await ethers.getContractFactory("SimpleAuction");
 
-  const url = getNetworkUrl();
-
-  const web3 = new Web3(new Web3.providers.HttpProvider(url));
-
-  const signer = web3.eth.accounts.privateKeyToAccount(
-    String(process.env.SIGNER_PRIVATE_KEY)
+  // Start deployment, returning a promise that resolves to a contract object
+  const simpleAuction = await SimpleAuction.deploy(
+    auctionEndTime,
+    beneficiaryAddress
   );
-  web3.eth.accounts.wallet.add(signer);
-
-  const contract = new web3.eth.Contract(abi);
-  // @ts-ignore
-  contract.options.data = bytecode;
-
-  const deployTx = contract.deploy({
-    data: bytecode,
-    arguments: [90000000000000, signer.address],
-  });
-
-  const estimatedGas = await deployTx.estimateGas();
-
-  const deployedContract = await deployTx
-    .send({
-      from: signer.address,
-      gas: estimatedGas.toString(),
-    })
-
-    .once("transactionHash", (txhash) => {
-      console.log(`Mining deployment transaction ...`);
-      console.log(`https://${network}.etherscan.io/tx/${txhash}`);
-    });
-
-  console.log(`Contract deployed at ${deployedContract.options.address}`);
   console.log(
-    `Add contract address to the.env file to store the contract address: ${deployedContract.options.address}`
+    "Contract deployed to address:",
+    await simpleAuction.getAddress()
   );
-};
+}
 
-deploy();
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
