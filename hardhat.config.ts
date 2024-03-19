f69cbc5c1c5b9e28ada8243e394a9f6fceeb211f
philipszdavido <kurtwanger40@gmail.com>
2024-03-19 09:41:50 +0100

    Retrieves env vars from dotenv

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,10 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { ETHEREUM_NETWORK, SIGNER_PRIVATE_KEY } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
