dbc6d816174e66b4f29741225d568a04adae3dfd
philipszdavido <kurtwanger40@gmail.com>
2024-03-20 23:49:27 +0100

    Implemented a /deploy endpoint to deploy a new auction smart contract

--- src/routes/auctionRoutes.ts
+++ src/routes/auctionRoutes.ts
@@ -4,6 +4,7 @@ import {
   status,
   history,
   statistics,
+  deploy,
 } from "../controllers/auctionController";
 import { verifyTokenMiddleware } from "../middlewares/authMiddleware";
 
@@ -13,4 +14,5 @@ router.get("/status", verifyTokenMiddleware, status);
 router.get("/history", verifyTokenMiddleware, history);
 router.post("/submit-bid", verifyTokenMiddleware, submitBid);
 router.get("/statistics", verifyTokenMiddleware, statistics);
+router.post("/deploy", verifyTokenMiddleware, deploy);
 export default router;
